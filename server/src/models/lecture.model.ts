aeb082ea6f612d7578246b4370b241a3ab899a29
unknown <tokiosman96@gmail.com>
2025-08-15 00:13:24 +0600

    feat(model):add Lecture Schema and interface

--- server/src/models/lecture.model.ts
+++ server/src/models/lecture.model.ts
@@ -1,1 +1,22 @@
 import mongoose,{Document} from "mongoose";
+import { type } from "os";
+
+
+export interface ILecture extends Document{
+    title:string;
+    vedioUrl:string;
+    pdfNote:string[];
+    module:mongoose.Types.ObjectId;
+    order:Number;
+}
+
+const lectureSchema= new mongoose.Schema({
+    title:{type:String,required:true},
+    vedioUrl:{type:String,required:true},
+    pdfNote:[{type:String}],
+    module:{type:mongoose.Schema.Types.ObjectId,ref:'Module',require:true},
+    order:{type:Number,require:true}
+
+})
+
+export default mongoose.model<ILecture>("Lecture",lectureSchema)
